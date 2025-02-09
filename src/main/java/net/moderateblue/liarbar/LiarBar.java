d510aa4d589b0881414b948fbe1089927ebb5f6e
moderateblue <sixinyangt@gmail.com>
2025-02-08 23:08:00 -0500

    Initial commit

---
+++ src/main/java/net/moderateblue/liarbar/LiarBar.java
@@ -0,0 +1,17 @@
+package net.moderateblue.liarbar;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LiarBar implements ModInitializer {
+	public static final String MOD_ID = "liarbar";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/moderateblue/liarbar/LiarBarClient.java
@@ -0,0 +1,11 @@
+package net.moderateblue.liarbar;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LiarBarClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
